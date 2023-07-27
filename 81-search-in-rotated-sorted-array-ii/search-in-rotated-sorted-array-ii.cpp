cd683ad58e22a18b1bc3d70eaaf7800667742f0c
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2023-07-27 20:44:04 +0530

    Time: 5 ms (67.65%) | Memory: 13.9 MB (50.58%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]==k) return true;
