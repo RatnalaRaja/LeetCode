2ddb2445487bb17ca109252ff7fc91ec303fb959
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2023-07-27 21:46:00 +0530

    Time: 193 ms (21.41%) | Memory: 14.9 MB (37.87%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,28 +1,21 @@
-class Solution{ 
-    private:
-bool searchVector(vector<int>& v, int target){
-    int left = 0, right = v.size() - 1;
-
-    while (left <= right) {
-	int mid = left + (right - left) / 2;
-	if (v[mid] == target)
-		return true;
-	if (v[mid] < target)
-		left = mid + 1;
-	else
-		right = mid - 1;
-    }
-
-    return false;
- }
-public:
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    if (matrix.size()==0) 
+class Solution {
+private:
+    bool binary(vector<int>&v,int k){
+        int s=0,n=v.size(),e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(v[mid]==k) return true;
+            else if(v[mid]<k) s=mid+1;
+            else e=mid-1;
+        }
         return false;
-    for(int i=0; i<matrix.size(); i++){
-        if( searchVector(matrix[i], target) ) 
-            return true;
     }
-    return false;
- }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size()==0) return false;
+        for(int i=0;i<matrix.size();i++){
+            if(binary(matrix[i],target)) return true;
+        }
+        return false;
+    }
 };
