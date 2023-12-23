a53afcabd07d5276b67e0c9c4cc0ea1fe44cee36
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2023-12-23 06:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (35.91%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    bool isPathCrossing(string& path) {
+        unordered_set<int> P;
+        int x0 =10000, y0=10000, xy=x0*20001+y0;
+        P.insert(xy);
+
+        for (char c : path) {
+            switch (c) {
+                case 'N': y0++; break;
+                case 'S': y0--; break;
+                case 'E': x0++; break;
+                case 'W': x0--; break;
+            }
+            xy=x0*20001+y0;
+            if (P.count(xy)==1) return 1;
+            P.insert(xy);
+        }
+        return 0;    
+    }
+};
+
