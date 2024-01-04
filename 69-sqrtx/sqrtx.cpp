de27ea66a2c4a454be0204967ee456f7ad7b869a
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-04 22:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (35.82%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,13 +1,17 @@
 class Solution {
-    private:
-        int sqrte(int n,long long x){
-            if(x*x<=n) return x;
-            else return sqrte(n,(x+(n/x))/2);
-        }
 public:
     int mySqrt(int x) {
-        long long n=x;
-        int a=sqrte(x,n);
-        return a;
+        int s=0;
+        int e=x;
+        int ans=-1;
+        long long int mid=s+(e-s)/2;
+        if(x==0 or x==1) return x;
+        while(s<=e){
+            if((mid*mid)==x)return mid;
+            if ((mid*mid)>x)e=mid-1;
+            else  {ans=mid;s=mid+1;}
+            mid=s+(e-s)/2;
+        }
+        return ans;
     }
 };
