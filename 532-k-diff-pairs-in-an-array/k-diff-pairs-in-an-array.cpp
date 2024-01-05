23a534f8c3f458f7268d896e83543cc662f4e7d1
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-05 17:47:23 +0530

    Time: 124 ms (10.31%) | Memory: 373.9 MB (5.21%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
-    sort(nums.begin(),nums.end());
-    set<pair<int,int>>s;
-    int i=0,j=1;
-    while(j<nums.size()){
-    int diff=nums[j]-nums[i];
-        if(diff==k){
-            s.insert({nums[i],nums[j]});
-        i++;
-        j++;
+    int binarysear(vector<int>v,int start,int target){
+        int e=v.size()-1;
+        int mid=start+(e-start)/2;
+        while(start<=e){
+            if(v[mid]==target){
+                return mid;
+            }
+            else if(v[mid]>target){
+                e=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            mid=start+(e-start)/2;
         }
-        else if(diff>k) i++;
-        else j++;
-        if(i==j)j++;
+        return -1;
+
     }
-        return s.size();
+    int findPairs(vector<int>& nu, int k) {
+        sort(nu.begin(),nu.end());
+        set<int>s;
+        for(int i=0;i<nu.size();i++){
+            if(binarysear(nu,i+1,nu[i]+k)!=-1)s.insert(nu[i]+k);
+        }
+    return s.size();
     }
 };
