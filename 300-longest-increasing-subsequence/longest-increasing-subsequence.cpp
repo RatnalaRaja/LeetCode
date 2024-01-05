e7fc5f717a48124534caf51e9edd1abeb059ae48
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-05 14:49:00 +0530

    Time: 3 ms (98.69%) | Memory: 11 MB (36.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        std::vector<int> tails(nums.size(), 0);
-        int size = 0;
-
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] < x)
-                    i = m + 1;
-                else
-                    j = m;
-            }
-            tails[i] = x;
-            if (i == size) ++size;
-        }
-        return size;
+       vector<int>v(nums.size(), 0);
+       int count=0;
+       for(int i:nums){
+           int s=0,e=count;
+           while(s!=e){
+               int mid=(s+e)/2;
+               if(v[mid]<i) s=mid+1;
+               else e=mid;
+           }
+           v[s]=i;
+           if(s==count)count++;
+       } 
+       return count;
     }
 };
-
-        
