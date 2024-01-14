30f484a99aaba42eb42f6fd1b604ddc990ce9bdc
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-14 18:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (81.08%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.cpp
+++ 728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    bool dividingnumber(int n){
-        int temp=n;
-        while(n>0){
-            int rem=n%10;
-            if(rem==0 or temp %rem!=0)return false;
-            n/=10;
-        }
-        return true;
-    }
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int>v;
-        for(int i=left;i<=right;i++){
-            if(dividingnumber(i))v.push_back(i);
+        for(int i=left;i<=right;i++)
+        {
+            int num=i;
+            int r=0;
+            int flag=0;
+            while(num>0)
+            {
+                r=num%10;
+                if( r==0 or i%r!=0 )
+                {
+                    flag=1;
+                   
+                }
+                num=num/10;
+            }
+            if(flag==0){
+                v.push_back(i);
+            }
         }
         return v;
     }
