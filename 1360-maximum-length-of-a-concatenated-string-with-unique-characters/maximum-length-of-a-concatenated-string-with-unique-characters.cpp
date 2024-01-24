d2325fd0b8d8ff02649aeb343d33295bc48c62b1
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-24 20:03:03 +0530

    Time: 5 ms (91.34%) | Memory: 10.3 MB (78.50%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
-
-    int maxi = 0;
-
-    bool isUnique(string s){
-
-        set<char> st;
-        
-        for(auto x : s){
-            st.insert(x);
-        }
-
-        if(s.size() == st.size()){
-            return true;
-        }
-        
-        return false;
-    }
-
-    void solve(vector<string>& arr, int i, int n, string temp){
-
-        
-        if(isUnique(temp) == false){
-            return;
-        }
-
-   
-        if(i >= n){
-
-            
-            int currSize = temp.size();
-            maxi = max(maxi, currSize);
-
-            return;
-        }
-
-     
-        solve(arr, i+1, n, temp + arr[i]);
-
-        
-        solve(arr, i+1, n, temp);
-    }
-
     int maxLength(vector<string>& arr) {
+        vector<int> dp = {0};
+        int res = 0;
         
-        int n = arr.size();
-        solve(arr, 0, n, ""); 
-
-        return maxi;
+        for (const string& s : arr) {
+            int a = 0, dup = 0;
+            for (char c : s) {
+                dup |= a & (1 << (c - 'a'));
+                a |= 1 << (c - 'a');
+            }
+            
+            if (dup > 0)
+                continue;
+            
+            for (int i = dp.size() - 1; i >= 0; i--) {
+                if ((dp[i] & a) > 0)
+                    continue;
+                dp.push_back(dp[i] | a);
+                res = max(res, __builtin_popcount(dp[i] | a));
+            }
+        }
+        
+        return res;
     }
 };
