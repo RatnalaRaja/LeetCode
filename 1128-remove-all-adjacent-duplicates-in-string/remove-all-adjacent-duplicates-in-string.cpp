08863d1cfcdb18d2c3de33915e6e5eef8c58b64c
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-02-09 00:18:19 +0530

    Time: 26 ms (39.61%) | Memory: 12.8 MB (21.32%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-	string removeDuplicates(string s) {
-
-		string ans;
-		ans.push_back(s[0]);
-
-		for(int i=1 ; i<s.length() ; i++){
-
-			if(!ans.empty() && s[i] == ans.back()){
-				ans.pop_back();
-			}
-			else{
-				ans.push_back(s[i]);
-			}
-		}
-
-		return ans;
-	}
+    string removeDuplicates(string s) {
+        stack<char>st;
+       for(int i=0;i<s.length();i++){
+           if(st.empty()){
+               st.push(s[i]);
+           }
+           else if(!st.empty() and st.top()==s[i]){
+               st.pop();
+           }
+           else{
+               st.push(s[i]);
+           }
+       }
+       string ans;
+       while(!st.empty()){
+           ans+=st.top();
+           st.pop();
+       }
+        reverse(begin(ans),end(ans));
+        return ans;
+    }
 };
