afb988f1e1a6569ff267969310f6fcddaa1becc3
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-02-19 02:14:47 +0530

    Time: 6 ms (57.83%) | Memory: 9.1 MB (61.66%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isvalid(int n,vector<string>&queen,int col,int row){
-         for (int c = 0; c < col; c++) {
-            if (queen[row][c] == 'Q')
-                return false;
+          for(int c=col;c>=0;c--){
+            if(queen[row][c]=='Q')return false;
         }
         for(int i=row,j=col;i>=0 and j>=0 ;i--,j--){
             if(queen[i][j]=='Q')return false;
