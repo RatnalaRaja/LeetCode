e7ba076bd460b5e854a6521e7121b2741e7a43d3
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-02-19 10:05:19 +0530

    Time: 6 ms (57.83%) | Memory: 9.3 MB (43.06%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,35 +1,72 @@
 class Solution {
 public:
-    bool isvalid(int n,vector<string>&queen,int col,int row){
-          for(int c=col;c>=0;c--){
-            if(queen[row][c]=='Q')return false;
+    bool isvalid(int n,vector<string>&board,int row,int col){
+        if (board.empty()) return false;
+         for (int c = col; c >= 0; c--) {
+            if (board[row][c] == 'Q') return false;
         }
-        for(int i=row,j=col;i>=0 and j>=0 ;i--,j--){
-            if(queen[i][j]=='Q')return false;
+        for(int i=row,j=col;i>=0 and j>=0;i--,j--){
+            if(board[i][j]=='Q')return false;
         }
         for(int i=row,j=col;i<n and j>=0;i++,j--){
-            if(queen[i][j]=='Q')return false;
+            if(board[i][j]=='Q')return false;
         }
         return true;
     }
-
-    void Queen(int n,vector<vector<string>>&board,vector<string>&str,int col){
+    void placedqueen(int n,vector<vector<string>>&board,vector<string>&str,int col){
         if(col==n){
             board.push_back(str);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isvalid(n,str,col,row)){
+            if(isvalid(n,str,row,col)){
                 str[row][col]='Q';
-                Queen(n,board,str,col+1);
+                placedqueen(n,board,str,col+1);
                 str[row][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>board;
-        vector<string>str(n,string(n,'.'));
-        Queen(n,board,str,0);
+        vector<string>checks(n,string(n,'.'));
+        placedqueen(n,board,checks,0);
         return board;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isvalid(int n,vector<string>&queen,int col,int row){
+//           for(int c=col;c>=0;c--){
+//             if(queen[row][c]=='Q')return false;
+//         }
+//         for(int i=row,j=col;i>=0 and j>=0 ;i--,j--){
+//             if(queen[i][j]=='Q')return false;
+//         }
+//         for(int i=row,j=col;i<n and j>=0;i++,j--){
+//             if(queen[i][j]=='Q')return false;
+//         }
+//         return true;
+//     }
+
+//     void Queen(int n,vector<vector<string>>&board,vector<string>&str,int col){
+//         if(col==n){
+//             board.push_back(str);
+//             return;
+//         }
+//         for(int row=0;row<n;row++){
+//             if(isvalid(n,str,col,row)){
+//                 str[row][col]='Q';
+//                 Queen(n,board,str,col+1);
+//                 str[row][col]='.';
+//             }
+//         }
+//     }
+//     vector<vector<string>> solveNQueens(int n) {
+//         vector<vector<string>>board;
+//         vector<string>str(n,string(n,'.'));
+//         Queen(n,board,str,0);
+//         return board;
+//     }
+// };
