d4b8283d275cc1bf4a0d968476694ce98c6a493a
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-02-22 12:14:54 +0530

    Time: 16 ms (34.11%) | Memory: 17 MB (43.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -4,7 +4,7 @@ static bool cmp(pair<int,int >& a, pair<int,int >& b) {
     return (a.second>b.second);
 }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>v;
+        vector<int > v ;
         unordered_map<int,int>ma;
         for(auto &i: nums){
             ma[i]++;
@@ -16,6 +16,7 @@ static bool cmp(pair<int,int >& a, pair<int,int >& b) {
         sort(f.begin(),f.end(),cmp) ;
         for(auto &i: f) {
             if(k > 0)v.push_back(i.first) ;
+            if(k <= 0 ) break;
             k--;
         }
         return v;
