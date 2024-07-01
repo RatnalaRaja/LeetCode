43417bf9bee5d629dbbf0c94da21000d3f78b207
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-07-01 20:59:45 +0530

    Time: 149 ms (37.10%) | Memory: 126.3 MB (86.16%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,32 +1,20 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> v;
-        // Separate positive and negative numbers into two separate vectors
-        vector<int> positive;
-        vector<int> negative;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] > 0) {
-                positive.push_back(nums[i]);
+        vector<int>v(nums.size());
+         int posIndex = 0;
+        int negIndex = 1;
+
+        for (int num : nums) {
+            if (num > 0) {
+                v[posIndex] = num;
+                posIndex += 2;
             } else {
-                negative.push_back(nums[i]);
+                v[negIndex] = num;
+                negIndex += 2;
             }
         }
-        // Rearrange elements alternatively
-        int i = 0, j = 0;
-        while(i < positive.size() || j < negative.size()) {
-            if(i < positive.size()) {
-                v.push_back(positive[i]);
-                i++;
-            }
-            if(j < negative.size()) {
-                v.push_back(negative[j]);
-                j++;
-            }
-        }
+
         return v;
     }
 };
