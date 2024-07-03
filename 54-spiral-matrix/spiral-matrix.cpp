885bf10b63f9ac937c694e0925b959122928b1a0
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-07-03 21:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (86.17%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int left=0,right=matrix[0].size()-1;
-        int top=0,bottom=matrix.size()-1;
-        vector<int> res;
+        vector<int>v;
+        int m=matrix.size();
+        int n=matrix[0].size();
 
-        while(top<=bottom && left<=right){
-            // left->right  >>>>
-            for(int i=left;i<=right;i++){
-                res.push_back(matrix[top][i]);
-            }
-            top++;
-            // top->bottom vvvv
-            for(int i=top;i<=bottom;i++){
-                res.push_back(matrix[i][right]);
-            }
-            right--;
-            if(top<=bottom){
-                // right->left <<<<
-                for(int i=right;i>=left;i--){
-                    res.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            if(left<=right){
-                // bottom->top ^^^^
-                for(int i=bottom;i>=top;i--){
-                    res.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+        int total=m*n;
+
+        int startRow=0;
+        int endRow=m-1;
+        int startCol=0;
+        int endCol=n-1;
+
+        int count=0;
+        while(count<total)
+        { 
+      for(int i=startCol;i<=endCol and count<total;i++){
+            v.push_back(matrix[startRow][i]);
+            count++;
         }
-        return res;
+        startRow++;
+
+        for(int i=startRow;i<=endRow  and count<total;i++){
+            v.push_back(matrix[i][endCol]);
+              count++;
+        }
+        endCol--;
+
+        for(int i=endCol;i>=startCol  and count<total ;i--){
+            v.push_back(matrix[endRow][i]);
+              count++;
+        }
+        endRow--;
+        for(int i=endRow;i>=startRow  and count<total ;i--){
+            v.push_back(matrix[i][startCol]);
+              count++;
+        }
+        startCol++;
+
+        
+        
+
+
+
+        }
+
+
+
+
+        return v;
     }
 };
