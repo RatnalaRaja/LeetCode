ca2672b89ecdf5b5554c8aa84a25f26cdfae6a0c
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2025-02-23 23:34:52 +0530

    Time: 4 ms (37.38%) | Memory: 18.4 MB (23.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
+    static bool customSort(pair<int,int>&a,pair<int,int>&b){
+        if(a.second==b.second)return a.first<b.first;
+        return a.second>b.second;
+    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>v;
-        unordered_map<int,int>mp;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>miniHeap;
-        for(auto &i:nums){
-            mp[i]++;
-        }
-        for(auto i=mp.begin();i!=mp.end();i++){
-            miniHeap.push({i->second,i->first});
-            if(miniHeap.size()>k){
-                miniHeap.pop();
-            }
-        }
-        while(!miniHeap.empty()){
-            v.push_back(miniHeap.top().second);
-            miniHeap.pop();
-        }
-        return v;
+       unordered_map<int,int>mpp;
+       for(auto&i:nums)mpp[i]++;
+       vector<pair<int,int>>freq(mpp.begin(),mpp.end());
+       vector<int>v;
+       sort(freq.begin(),freq.end(), customSort);
+       for(int i=0;i<k;i++){
+            v.push_back(freq[i].first);
+       }
+       return v;
     }
 };
